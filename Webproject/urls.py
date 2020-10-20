6de094a5f868bc37c4dbbd01b65ec642a9af0d2e
samrat101 <samratrao101@gmail.com>
2020-10-20 20:36:34 +0530

    Models for groups and posts Implemented

--- Webproject/urls.py
+++ Webproject/urls.py
@@ -19,8 +19,10 @@ from django.conf.urls import url,include
 from . import views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
     url(r'^$',views.HomePage.as_view(),name='home'),
-    url(r'accounts/',include('accounts.urls'),namespace='accounts'),
+    url(r'^test/$',views.TestPage.as_view(),name='test'),
+    url(r'^thanks/$',views.ThanksPage.as_view(),name='thanks'),
+    path('admin/', admin.site.urls),
+    url(r'accounts/',include('accounts.urls',namespace='accounts')),
     url(r'accounts/',include('django.contrib.auth.urls')),
 ]
--- accounts/urls.py
+++ accounts/urls.py
@@ -1,5 +1,5 @@
 from django.conf.urls import url
-form django.contrib.auth import views as auth_views
+from django.contrib.auth import views as auth_views
 from . import views
 
 app_name='accounts'
